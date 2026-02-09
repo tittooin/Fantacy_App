7786755eeba2afad00912583d5321ebdf2583cdf
Hugging Face CLI <user@example.com>
2026-02-09 17:51:24 +0530

    Removed KYC UI from Profile & Updated Pubspec

--- lib/features/wallet/presentation/redeem_screen.dart
+++ lib/features/wallet/presentation/redeem_screen.dart
@@ -1,8 +1,6 @@
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class RedeemScreen extends StatefulWidget {
